dee137c1c8f6e96173f93342728e5cd42eb5ff6f
Duy Hoang <hoangdduy1024@gmail.com>
2024-04-24 21:57:27 +0700

    Development & Production Error

--- Controllers/error.controller.js
+++ Controllers/error.controller.js
@@ -1,10 +1,33 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500
-  err.status = err.status || 'error'
+const devErrors = (res, error) => {
+	res.status(error.statusCode).json({
+		status: error.status,
+		message: error.message,
+		stackTrace: error.stack,
+		error: error,
+	})
+}
 
-  res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-  })
+const prodErrors = (res, error) => {
+	if (error.isOperational) {
+		res.status(error.statusCode).json({
+			status: error.status,
+			message: error.message,
+		})
+	} else {
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!'
+    })
+  }
+}
 
+module.exports = (error, req, res, next) => {
+	error.statusCode = error.statusCode || 500
+	error.status = error.status || 'error'
+
+	if (process.env.NODE_ENV === 'development') {
+		devErrors(res, error)
+	} else if (process.env.NODE_ENV === 'production') {
+		prodErrors(res, error)
+	}
 }
